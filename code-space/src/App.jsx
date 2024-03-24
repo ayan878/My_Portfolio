92d75b08b77f05b086e1c660be845a71bcc21d9b
ayan878 <ayanraza197@gmail.com>
2024-03-24 19:55:56 +0530

    Assets

--- code-space/src/App.jsx
+++ code-space/src/App.jsx
@@ -1,14 +1,19 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import AppLayout from "./components/AppLayout"
 import Home from "./components/Home"
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
-const router=createBrowserRouter([{
-  element:<AppLayout/>,
-  children:[
-    {path:'/',
-    element:<Home/>},
-  ]
-}])
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/skills", element: <Skills /> },
+      { path: "/projects", element: <Projects /> },
+    ],
+  },
+]);
 function App() {
   return (
 <RouterProvider router={router}/>
