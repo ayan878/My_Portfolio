0b1c5b5b9cd95351366c637dcd59b29ddddaef92
ayan878 <ayanraza197@gmail.com>
2024-03-24 19:56:35 +0530

    Nav

--- code-space/src/components/NavBar.jsx
+++ code-space/src/components/NavBar.jsx
@@ -1,18 +1,52 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import LinkedIn from "../assets/img/nav-icon1.svg";
+import Facebook from "../assets/img/nav-icon2.svg";
+import Twitter from "../assets/img/nav-icon3.svg";
+import Logo from "../assets/img/codeSpace.png";
 
 export default function NavBar() {
   return (
-    <>
-      <nav className="container flex justify-around py-8 mx-auto bg-white">
-        <div>
-          <h3 className="text-2xl font-medium text-blue-500">LOGO</h3>
+    <nav className="container flex justify-between items-center py-8 mx-auto mb-16">
+      <div className="flex space-x-4">
+        <img src={Logo} alt="Logo" className="w-14 h-14" /><span className="text-4xl text-white font-bold">codeSpace</span>
+      </div>
+      <div className="flex space-x-8 items-center">
+        <NavLink to="/" className="text-white">
+          Home
+        </NavLink>
+        <NavLink to="/skills" className="text-white">
+          Skills
+        </NavLink>
+        <NavLink to="/projects" className="text-white">
+          Projects
+        </NavLink>
+        <div className="flex space-x-2 items-center">
+          <Link to="https://www.linkedin.com/login">
+            <img
+              src={LinkedIn}
+              alt="LinkedIn"
+              className="w-8 h-8 rounded-full border border-gray-200 px-2.5"
+            />
+          </Link>
+          <Link to="https://www.facebook.com">
+            <img
+              src={Facebook}
+              alt="Facebook"
+              className="w-8 h-8 rounded-full border border-gray-200 px-2 "
+            />
+          </Link>
+          <Link to="https://www.twitter.com">
+            <img
+              src={Twitter}
+              alt="Twitter"
+              className="w-8 h-8 rounded-full border border-gray-200 px-2"
+            />
+          </Link>
         </div>
-        <div className="space-x-8">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact US</NavLink>
-        </div>
-      </nav>
-    </>
+        <Link className="border border-gray-200 px-4 py-2 rounded-md text-white font-semibold">
+          Let's Connect
+        </Link>
+      </div>
+    </nav>
   );
 }
