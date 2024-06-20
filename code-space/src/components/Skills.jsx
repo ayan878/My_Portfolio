5dd6d71a25a23c2c7978df81ab22cb60bae1e006
ayan878 <ayanraza197@gmail.com>
2024-06-21 00:24:03 +0530

    Skills

--- code-space/src/components/Skills.jsx
+++ code-space/src/components/Skills.jsx
@@ -2,11 +2,12 @@ import GreaterThan from "../assets/img/arrow2.svg";
 import LessThan from "../assets/img/arrow1.svg";
 import nintyFive from "../assets/img/meter1.svg";
 import ImageSlider from "./ImageSlider";
-
+import SkillsImg from "../assets/img/Skills.png"
 function Skills() {
   return (
     <div
-      className=" bg-black bg-opacity-80 mx-28 rounded-3xl py-4 "
+      // className=" bg-black bg-opacity-80 mx-28 rounded-3xl py-4 "
+      className="flex flex-col items-center justify-center"
       style={{
         boxShadow:
           "6px 6px -1px rgba(0,0,0,0.15), -6px -6px 10px rgba(255,255,255,0.7)",
@@ -20,37 +21,8 @@ function Skills() {
         possimus. Nihil esse soluta sint minima nemo ratione consectetur,
         delectus veniam?
       </h3>
-  <ImageSlider/>
-      {/* <div className="flex justify-between">
-        <button>
-          <img
-            src={LessThan}
-            alt="lessThan"
-            className="w-10 h-10 p-2 relative top-10 right-4 border rounded-full bg-purple-800"
-          />
-        </button>
-        <button>
-          <img
-            src={GreaterThan}
-            alt="GreaterThan"
-            className="w-10 h-10 p-2 relative top-10 left-4 border rounded-full bg-purple-800"
-          />
-        </button>
-      </div>
-      <div className="flex gap-16 mx-8 p-4 justify-center">
-        <div className="justify-center">
-          <img src={nintyFive} alt="95%Img" className="w-36" />
-          <h3 className="text-white text-3xl font-bold">JavaScript</h3>
-        </div>
-        <div>
-          <img src={nintyFive} alt="95%Img" className="w-36" />
-          <h3 className="text-white text-3xl font-bold">JavaScript</h3>
-        </div>
-        <div>
-          <img src={nintyFive} alt="95%Img" className="w-36" />
-          <h3 className="text-white text-3xl font-bold">JavaScript</h3>
-        </div>
-      </div> */}
+  {/* <ImageSlider/> */}
+      <img src={SkillsImg} alt="skills"/>
     </div>
   );
 }
