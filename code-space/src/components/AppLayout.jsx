556d9623ffd784576f69fa9dddba830bc64a08d1
ayan878 <ayanraza197@gmail.com>
2024-06-21 23:07:31 +0530

    Layout

--- code-space/src/components/AppLayout.jsx
+++ code-space/src/components/AppLayout.jsx
@@ -1,13 +1,50 @@
-import { Outlet } from "react-router-dom"
-import NavBar from "./NavBar"
+// import { Outlet } from "react-router-dom"
+// import NavBar from "./NavBar"
+// import Particle from "./Particle";
+
+// function AppLayout() {
+//     return (
+//       <div className="bg-[url('./src/assets/img/banner-bg.png')] h-screen bg-cover bg-center">
+//         <Particle/>
+//         <NavBar />
+//         <Outlet />
+//       </div>
+//     );
+// }
+
+// export default AppLayout
+
+// AppLayout.js
+// import { Outlet } from "react-router-dom";
+
+import Home from "./Home/Home";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import AboutMe from "./AboutMe";
+import Particle from "./Particle";
+import NavBar from "./NavBar/NavBar";
 
 function AppLayout() {
-    return (
-      <div className="bg-[url('./src/assets/img/banner-bg.png')] h-screen bg-cover bg-center">
-        <NavBar />
-        <Outlet />
+  return (
+    <div className="bg-[url('./src/assets/img/banner-bg.png')] h-screen bg-cover bg-center overflow-auto">
+      <Particle />
+      <NavBar />
+      <div>
+        <section id="home">
+          <Home />
+        </section>
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="about">
+          <AboutMe />
+        </section>
       </div>
-    );
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
