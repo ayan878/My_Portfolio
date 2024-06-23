44082de25288365cf69f17e78c8d758d78ef025f
ayan878 <ayanraza197@gmail.com>
2024-06-23 23:51:31 +0530

    setup

--- scrollapp/src/App.jsx
+++ scrollapp/src/App.jsx
@@ -4,13 +4,13 @@ import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
 import Contact from "./Contact";
-import "./App.css";
 import MobileNavbar from "./MobileNavbar";
 
 function App() {
   return (
     <div className="App">
       <div className="hidden md:block">
+        <h1 className="text-red-300">Hello</h1>
       <Navbar />
       </div>
       <div className="md:hidden"><MobileNavbar/></div>
