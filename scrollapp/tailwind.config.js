e40b5c7b3186ff4d8925c70959a07f9c8e505911
ayan878 <ayanraza197@gmail.com>
2024-06-24 15:06:11 +0530

    style

--- scrollapp/tailwind.config.js
+++ scrollapp/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        spaceGrotest:["Space Grotest", "sans-sarif"],
+      },
+    },
   },
   plugins: [],
 };
