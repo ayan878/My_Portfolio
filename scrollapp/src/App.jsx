7107dc8b5a1ef491fcd497b48568b8cb013fb7f2
ayan878 <ayanraza197@gmail.com>
2024-06-26 21:49:36 +0530

    App

--- scrollapp/src/App.jsx
+++ scrollapp/src/App.jsx
@@ -1,20 +1,23 @@
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import MobileNavbar from "./components/MobileNavbar";
-import HomeDesktop from "./pages/Home";
 import Skills from "./pages/Skills";
+import { Contact } from "./pages/Contact";
+import FloatingNavbar from "./components/FloatingNavbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 
 function App() {
+ 
   return (
     <div className="bg-[url('./src/assets/img/banner-bg.png')] h-screen bg-cover bg-center">
-      <MobileNavbar />
-      <HomeDesktop />
-      <Skills/>
+      <FloatingNavbar />
+      <Home />
+      <Contact />
+      <Skills />
       <About />
       <Services />
-      <Contact />
+      <Footer/>
     </div>
   );
 }
