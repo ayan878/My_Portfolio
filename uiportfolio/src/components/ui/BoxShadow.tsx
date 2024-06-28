1aef430728f1a89fe9b510b9add960f280fe558f
ayan878 <ayanraza197@gmail.com>
2024-06-28 23:41:14 +0530

    ui

--- uiportfolio/src/components/ui/BoxShadow.tsx
+++ uiportfolio/src/components/ui/BoxShadow.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 
 function BoxShadow() {
   return (
-    <div className="bg-white flex items-center justify-between">
-      <div className="rounded-full justify-between flex px-6 py-2 font-bold bg-indigo-500 text-white w-fit transition-all shadow-[3px_6px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
+    <div className="bg-white flex items-center justify-between w-96 h-16 ">
+      <div className="rounded-full text-2xl justify-between flex px-6 py-2 font-bold bg-indigo-500 text-white w-fit transition-all shadow-[3px_6px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
         Welcome to my codeSpace
       </div>
     </div>
--- uiportfolio/src/components/ui/BoxShadowButton.tsx
+++ uiportfolio/src/components/ui/BoxShadowButton.tsx
@@ -1,10 +1,34 @@
-
+import { FiArrowRight } from "react-icons/fi";
+import { useAnimate } from "framer-motion";
 
 const NeuButton = () => {
+  const [scope, animate] = useAnimate();
+
+  const handleClick = () => {
+    animate([
+      [".button", { scale: 1, boxShadow: "none" }, { duration: 0.000001 }],
+      [".rightArrow", { rotate: -25 }, { duration: 0.1, at: "<" }],
+
+      [".rightArrow", { rotate: 0 }, { duration: 0.1 }],
+      [
+        "button",
+        { scale: 1, boxShadow: "3px 5px 0px black" },
+        { duration: 0.1 },
+      ]
+    ]);
+  };
+
   return (
-    <div className="bg-white flex items-center justify-between">
-      <button className=" rounded-full justify-between flex px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
-        Hire Me 
+    <div
+      ref={scope}
+      className="bg-white flex items-center justify-between w-64"
+    >
+      <button
+        onClick={handleClick}
+        className="button text-2xl font-outfit items-center gap-2 rounded-full justify-between flex px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_5px_0px_black]"
+      >
+        Hire Me
+        <FiArrowRight onClick={handleClick} className="rightArrow" />
       </button>
     </div>
   );
