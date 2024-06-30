2218b51093cbf0931b42acd3b9301f49a8bd1cbf
ayan878 <ayanraza197@gmail.com>
2024-06-30 17:45:51 +0530

    floating navbar

--- uiportfolio/src/components/FloatingNavbar.tsx
+++ uiportfolio/src/components/FloatingNavbar.tsx
@@ -1,10 +1,16 @@
 import { Link as ScrollLink } from "react-scroll";
-import { FaHome, FaCodeBranch } from "react-icons/fa";
+import { FaHome, FaCodeBranch, FaUser } from "react-icons/fa";
 import { LuBrainCog } from "react-icons/lu";
+import { motion } from "framer-motion";
+import { useAnimatedBackgroundColor } from "./utils/backgroundUtils";
 
 function FloatingNavbar() {
+  const { backgroundColor } = useAnimatedBackgroundColor();
   return (
-    <div className="fixed top-1/2 right-8 md:right-12 transform -translate-y-1/2 flex flex-col w-7 md:w-10 bg-purple-900 justify-evenly items-center gap-4 rounded-full z-50">
+    <motion.div
+      className="fixed top-1/2 right-8 md:right-12 transform -translate-y-1/2 flex flex-col w-7 md:w-10 justify-evenly items-center gap-4 rounded-full z-50 shadow-[3px_6px_0px_black]"
+      style={{ backgroundColor }}
+    >
       <ScrollLink
         to="home"
         spy={true}
@@ -14,7 +20,7 @@ function FloatingNavbar() {
         activeClass="active-link"
         className="text-white p-2 cursor-pointer"
       >
-        <FaHome className="text-sm md:text-2xl" />
+        <FaHome className="text-sm md:text-2xl text-yellow-300" />
       </ScrollLink>
       <ScrollLink
         to="skills"
@@ -25,7 +31,19 @@ function FloatingNavbar() {
         activeClass="active-link"
         className="text-white p-2 cursor-pointer"
       >
-        <LuBrainCog  className="text-sm md:text-2xl" />
+        <LuBrainCog className="text-sm md:text-2xl text-yellow-300" />
+      </ScrollLink>
+
+      <ScrollLink
+        to="projects"
+        spy={true}
+        smooth={true}
+        offset={-50}
+        duration={500}
+        activeClass="active-link"
+        className="text-white p-2 cursor-pointer"
+      >
+        <FaCodeBranch className="text-sm md:text-2xl text-yellow-300" />
       </ScrollLink>
       <ScrollLink
         to="about"
@@ -36,20 +54,9 @@ function FloatingNavbar() {
         activeClass="active-link"
         className="text-white p-2 cursor-pointer"
       >
-        <FaCodeBranch className="text-sm md:text-2xl" />
+        <FaUser className="text-sm md:text-2xl text-yellow-300" />
       </ScrollLink>
-      <ScrollLink
-        to="contact"
-        spy={true}
-        smooth={true}
-        offset={-50}
-        duration={500}
-        activeClass="active-link"
-        className="text-white p-2 cursor-pointer"
-      >
-        <FaCodeBranch className="text-sm md:text-2xl" />
-      </ScrollLink>
-    </div>
+    </motion.div>
   );
 }
 
