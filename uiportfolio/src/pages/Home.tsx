38a9c97033124b3e1c7c0a4b52980823f4b37e48
ayan878 <ayanraza197@gmail.com>
2024-07-01 22:31:21 +0530

    fixed

--- uiportfolio/src/pages/Home.tsx
+++ uiportfolio/src/pages/Home.tsx
@@ -110,7 +110,7 @@ const Home:React.FC=()=> {
       <Navbar />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-0 px-4 md:px-12 ">
         <div className="p-8 md:pl-16 md:pr-8">
-          <h1 className="text-5xl md:text-5xl font-extrabold font-paytone tracking-wide md:mt-4">
+          <h1 className="text-2xl sm:text-3xl md:text-5xl font-extrabold font-paytone tracking-wide md:mt-4">
             WELCOME, to my CodeSpace
           </h1>
           <TypeAnimation
