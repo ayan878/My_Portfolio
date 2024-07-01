cbe2d02a55df9c71740f8d8f6a5fc2f6ba1ff871
ayan878 <ayanraza197@gmail.com>
2024-07-01 17:19:51 +0530

    fixed

--- uiportfolio/src/components/ui/BoxShadowButton.tsx
+++ uiportfolio/src/components/ui/BoxShadowButton.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { useAnimate } from "framer-motion";
 
-interface NeuBtn {
+interface NeuBtnProps {
   name: string;
 }
 
-const NeuButton: React.FC<NeuBtn> = ({ name }) => {
+const NeuButton: React.FC<NeuBtnProps> = ({ name }) => {
   const [scope, animate] = useAnimate();
 
   const handleClick = () => {
     animate([
       [".button", { scale: 1, boxShadow: "none" }, { duration: 0.1 }],
       [".button", { rotate: 0 }, { duration: 0.1, at: "<" }],
-
       [".button", { rotate: 0 }, { duration: 0.1 }],
       [
         "button",
@@ -23,16 +22,15 @@ const NeuButton: React.FC<NeuBtn> = ({ name }) => {
   };
 
   return (
-    <div
-      ref={scope}
-      className="bg-white flex items-center justify-between w-64"
-    >
-      <button
-        onClick={handleClick}
-        className="button text-2xl font-outfit items-center gap-2 mx-auto rounded-full justify-between flex px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_5px_0px_black]"
-      >
-        {name}
-      </button>
+    <div className="bg-white flex items-center justify-between w-64">
+      <div ref={scope}>
+        <button
+          onClick={handleClick}
+          className="button text-2xl font-outfit items-center gap-2 mx-auto rounded-full justify-between flex px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_5px_0px_black]"
+        >
+          {name}
+        </button>
+      </div>
     </div>
   );
 };
